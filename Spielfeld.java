157a35e504c5c15eb826f537a30c27c7d55b9514
N <nick3901@icloud.com>
2020-11-05 08:29:20 +0100

    neu

--- Spielfeld.java
+++ Spielfeld.java
@@ -62,6 +62,7 @@ public class Spielfeld
     private boolean pruefeVierInEinerSpalte ()
     {
         boolean erg = false;
+        //Diese Methode übernimmt Nick
         return erg;
     }       
 
